badf60809c4fd7b77edd11dfc9cd2d3a067932d4
Nikhil Tomar <ntomer747@gmail.com>
2025-11-28 09:46:42 +0530

    Fix currentUser export and image path

--- src/Data/currentUser.js
+++ src/Data/currentUser.js
@@ -6,7 +6,7 @@ export const currentUser = {
   website: "nikhiltomar.dev",
   
   // ✅ CHANGE YOUR IMAGE HERE ONLY - IT WILL UPDATE EVERYWHERE
-  image: "/image/my img-2.jpg", 
+  image: "public/image/my-img-2.jpg", 
   
   cover: "https://images.pexels.com/photos/1629236/pexels-photo-1629236.jpeg?auto=compress&cs=tinysrgb&w=1200",
   stats: { posts: "86", followers: "12.5k", following: "450" }
